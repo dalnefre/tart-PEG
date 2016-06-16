e548233eb16cb839d704884da36e227fb792f98e
Dale Schumacher <dale.schumacher@gmail.com>
2016-06-16 09:41:27 -0500

    Refactor parseSyntax and clean up old code

--- examples/humus.js
+++ examples/humus.js
@@ -69,14 +69,27 @@ var parseTokens = function parseTokens(source) {
         humusTokens.call('tokens'),
         sponsor(function okBeh(m) {
             log('Tokens OK:', JSON.stringify(m, null, '  '));
-            var tokens = m.value;
-            dumpTokens(tokens);
-//            parseSyntax(tokens);
+            parseSyntax(m.value);
         }),
         sponsor(function failBeh(m) {
             console.log('Tokens FAIL:', JSON.stringify(m, null, '  '));
         })
     ));
+    source(start);
+};
+
+var parseSyntax = function parseSyntax(tokens) {
+    dumpTokens(tokens);
+    var source = input.fromArray(sponsor, tokens);
+    var start = sponsor(PEG.start(
+        humusSyntax.call('humus'),
+        sponsor(function okBeh(m) {
+            console.log('Syntax OK:', JSON.stringify(m, null, '  '));
+        }),
+        fail: sponsor(function failBeh(m) {
+            console.log('Syntax FAIL:', JSON.stringify(m, null, '  '));
+        })
+    });
     source(start);
 };
 
@@ -88,86 +101,7 @@ var dumpTokens = function dumpTokens(list) {
     process.stdout.write('</TOKENS>\n');
 };
 
-var parseSyntax = function parseSyntax(tokens) {
-    var start = humusSyntax.call('humus');
-    start({
-        input: input.fromArray(sponsor, tokens),
-        ok: sponsor(function failBeh(m) {
-            console.log('Syntax FAIL:', JSON.stringify(m, null, '  '));
-        }),
-        fail: sponsor(function failBeh(m) {
-            console.log('Syntax FAIL:', JSON.stringify(m, null, '  '));
-        })
-    });
-};
-
 parseTokens(source);
-
-/*
-var parseTokens = function parseTokens(source) {
-    var ns = require('./humusTokens.js').build(this.sponsor, log);
-    require('./reduceTokens.js').transform(ns);
-
-    var ok = this.sponsor(function okBeh(m) {
-        log('Tokens OK:', JSON.stringify(m, null, '  '));
-        var syntax = this.sponsor(parseSyntax);
-        syntax(m.value);
-    });
-    var fail = this.sponsor(function failBeh(m) {
-        console.log('Tokens FAIL:', JSON.stringify(m, null, '  '));
-    });
-
-    var stream = sponsor(require('../input.js').stringStream(source));
-    var start = sponsor(ns.lookup('tokens'));
-    var matcher = sponsor(PEG.start(start, ok, fail));
-    stream(matcher);
-};
-
-var parseSyntax = function parseSyntax(source) {
-    var ns = require('./humusSyntax.js').build(this.sponsor, log);
-//    require('./reduceSyntax.js').transform(ns);
-
-    process.stdout.write('<TOKENS>\n');
-    var list = source;
-    for (var i = 0; i < list.length; ++i) {
-        process.stdout.write(JSON.stringify(list[i]) + '\n');
-    }
-    process.stdout.write('</TOKENS>\n');
-
-    var ok = this.sponsor(function okBeh(m) {
-        console.log('Syntax OK:', JSON.stringify(m, null, '  '));
-    });
-    var fail = this.sponsor(function failBeh(m) {
-        console.log('Syntax FAIL:', JSON.stringify(m, null, '  '));
-    });
-
-    var stream = sponsor(require('../input.js').arrayStream(source));
-    var start = sponsor(ns.lookup('humus'));
-    var matcher = sponsor(PEG.start(start, ok, fail));
-    stream(matcher);
-};
-
-var helloSource = 
-    'SEND (#Hello, "World", \'\\n\', ##, -16#2a) TO println\n';
-var labelSource = 
-    'LET label_beh(cust, label) = \\msg.[ SEND (label, msg) TO cust ]\n'
-  + 'CREATE R WITH label_beh(println, #Right)\n'
-  + 'CREATE L WITH label_beh(println, #Left)\n'
-  + 'SEND #Hello TO R\n';
-  + 'SEND #Hello TO L\n';
-//var fileSource = require('fs').readFileSync('sample.hum', 'utf8');
-
-var parser = sponsor(parseTokens);
-parser(helloSource);
-*/
-
-/*
-tracing.eventLoop({
-    fail: function (e) {
-        console.log('ERROR!', e);
-    }
-});
-*/
 
 require('../fixture.js').asyncRepeat(3,
     function eventLoop() {
